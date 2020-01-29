534e3b6491bd62a69f4d3c8cdfc8fc277b9f8ba4
Dmitriy Badovski <58221063+Dmitrichxd@users.noreply.github.com>
2020-01-29 16:15:15 +0300

    Добавил маркер на карту

--- force-app/main/default/aura/Map/MapController.js
+++ force-app/main/default/aura/Map/MapController.js
@@ -1,18 +1,23 @@
 ({
-    jsLoaded: function(component) {
+    jsLoaded: function (component) {
         component.set("v.jsLoaded", true);
-    } ,
-    
-    onPlotMapMarker: function(component,event) {
+        component.set("v.customMarker", {
+            iconUrl: 'resource/PushPin.png',
+            iconSize: [48, 48],
+            iconAnchor: [22, 94],
+            popupAnchor: [-3, -76],
+          /*  shadowUrl: '',
+            shadowSize: [68, 95],
+            shadowAnchor: [22, 94]*/
+        });
+    },
+
+    onPlotMapMarker: function (component, event) {
         var longitude = event.getParam("long");
         var latitude = event.getParam("lat");
-        component.set("v.location",[
-            {
-                Latitude: latitude,
-                Longitude: longitude
-            } 
-        ]);
-        console.log(latitude);
-        console.log(longitude);
-    } ,
+        component.set("v.location", {
+                latitude: latitude,
+                longitude: longitude                        
+            });
+    },
 })
--- force-app/main/default/aura/Map/MapRenderer.js
+++ force-app/main/default/aura/Map/MapRenderer.js
@@ -1,41 +1,46 @@
 ({
     rerender: function (component) {
-        
+
         var nodes = this.superRerender();
-        
+
         var location = component.get('v.location');
-		    
+        var pinIcon = L.icon({
+            iconUrl: '/resource/Icons/IconsLand/PNG/Centered/48x48/MapMarker_Ball_Right_Blue.png',
+            iconSize: [48, 48], // size of the icon
+            //iconAnchor: [0, 94], // point of the icon which will correspond to marker's location
+            popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+        });
         if (!location) {
-           
+
         } else {
             // If the Leaflet library is not yet loaded, we can't draw the map: return
             if (!window.L) {
                 return nodes;
             }
-            
+
             // Draw the map if it hasn't been drawn yet
             if (!component.map) {
                 var mapElement = component.find("map").getElement();
-                component.map = L.map(mapElement, {zoomControl: true}).setView([42.356045, -71.085650], 13);
+                component.map = L.map(mapElement, { zoomControl: true }).setView([42.356045, -71.085650], 13);
                 component.map.scrollWheelZoom.disable();
-                window.L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', {attribution: 'Tiles © Esri'}).addTo(component.map);
+                window.L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Street_Map/MapServer/tile/{z}/{y}/{x}', { attribution: 'Tiles © Esri' }).addTo(component.map);
             }
-            
-            
-            
-            if (location && location.lat && location.long) {
-                var latLng = [location.lat, location.long];
+
+
+
+            if (location && location.latitude && location.longitude) {
+                var latLng = [location.latitude, location.longitude];
                 if (component.marker) {
                     component.marker.setLatLng(latLng);
                 } else {
-                    component.marker = window.L.marker(latLng);
+                    component.marker = window.L.marker(latLng, { icon: pinIcon });
                     component.marker.addTo(component.map);
                 }
                 component.map.setView(latLng);
             }
-            
+
             return nodes;
         }
-        
+
     }
 })
